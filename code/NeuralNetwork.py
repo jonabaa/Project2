b3626429f95b19a9d27b7182d757a938df86dc3e
jeanpylon <jonaudunbaar@gmail.com>
2018-10-28 13:12:07 +0100

    fix NeuralNetwork.py

--- code/NeuralNetwork.py
+++ code/NeuralNetwork.py
@@ -1,16 +1,16 @@
 # This class implements a multilayered neural network
 
-from numpy import random, zeros, sum, random.choice
+from numpy import random, zeros, sum
+from numpy.random import choice
 
 class NeuralNetwork:
 
     def __init__(this):
+        this.number_of_nodes = [] # number of nodes in each layer
+        this.number_of_inputs = None # number of input nodes
+
         this.W = [] # list containing all the L weightmatrices
         this.b = [] # list containing all the L biasvectors
-        
-        # for updating iteratively
-        this.W_old = [] # list containing all the L weightmatrices
-        this.b_old = [] # list containing all the L biasvectors
         
         this.grad_W = [] # list containing gradients for the weightmatrices
         this.grad_b = [] # list containing gradients for the biasvectors
@@ -26,9 +26,9 @@ class NeuralNetwork:
         
         # flag determining wether the network can start to learn
         this.compiled = False 
-"""
+        """
         this.delta = None # the deltas in the backpropagation
-"""
+        """
 
     #
     # Functions for setting up the neural network
@@ -43,15 +43,18 @@ class NeuralNetwork:
     # @act_func_diff: The derivative of the activation function 
     #                 of this layer
     #
-    def add_layer(this, input_dim, output_dim, act_func, act_func_diff):
-        # set up the weights of the layer
-        this.W.append((random.random((output_dim, inpu_dim)) - .5)*.7/.5)
-        # set up the biases of the layer
-        this.b.append((random.random(output_dim) - .5)*.7/.5)
+    def add_layer(this, nodes, act_func, act_func_diff):
+        # store the number of nodes in the layer
+        this.number_of_nodes.append(nodes)
         # store the activation function of the layer
         this.act.append(act_func)
         # store the derivative of the activation function of the layer
         this.act_diff.append(act_func_diff)
+
+    
+    # set the number of input nodes in the network
+    def set_inputnodes(this, n):
+        this.number_of_inputs = n
 
 
     # Setting the cost function for the network
@@ -67,9 +70,7 @@ class NeuralNetwork:
     # This function must be run after the network is specified and before
     # training 
     def compile(this):
-        print("This function is not fully implemented yet.")
-        
-        if (len(this.W) == 0):
+        if (len(this.number_of_nodes) == 0):
             print("No layers. Compilation unsuccessfull.")
             return
 
@@ -78,10 +79,13 @@ class NeuralNetwork:
             return
 
         if (this.cost_func_diff == None):
-            print("Cost function derivative is missing. 
-                    Compilation unsuccessfull.")
+            print("Cost function derivative is missing. Compilation unsuccessfull.")
             return
         
+        if (this.number_of_inputs == None):
+            print("Number of inputnodes is missing. Compilation unsuccessfull.")
+            return
+        """
 
         # set up list of vectors of deltas
         this.delta = [zeros((len(this.b[0]), 1))]
@@ -91,7 +95,19 @@ class NeuralNetwork:
         # set up list of gradientmatrices C(W)
         
         # set up list of gradientvectors C(b)
+        """
 
+        # set up the weights and biases of the layers
+        n = this.number_of_nodes
+        inp = this.number_of_inputs
+
+        this.W.append((random.random((n[0], inp)) - .5)*.7/.5)
+        this.b.append((random.random((n[0], 1)) - .5)*.7/.5)
+        
+        for l in range(len(this.number_of_nodes)-1):
+            this.W.append((random.random((n[l+1],n[l]))-.5)*.7/.5)
+            this.b.append((random.random((n[l+1],1)) - .5)*.7/.5)
+        
         this.compiled = True
 
 
@@ -107,7 +123,7 @@ class NeuralNetwork:
     #     a column vector
     #
     def predict(this, x):
-        if !this.compiled:
+        if not this.compiled:
             print("Network is not compiled. Use compile().")
             return
         
@@ -124,106 +140,120 @@ class NeuralNetwork:
     # Also saves the activations, z, in the network for the given input
     #
     # @x: the input/predictor for the neural network to predict on as
-    #     a column vector
+    #     a row-vector
     #
     def forward(this, x):
-        if !this.compiled:
+        if not this.compiled:
             print("Network is not compiled. Use compile().")
             return
         
-        # set up list of vectors of activations
-        z = [zeros((len(this.b[0]), 1))]
-        for i in range(len(b) - 1):
-            z.append(zeros((len(this.b[i+1]), 1))]
+        L = len(this.b)
         
-        y = x
+        # set up list of vectors of activations
+        # MIGHT BE SIMPLIFIED SINCE THE ELEMENTS ARE replaced later
+        z = []
+        for l in range(L):
+            z.append(zeros((len(this.b[l]), 1)))
+        
 
         # compute the output
-        for i in range(len(this.W)):
-            z[i] = this.W[i].dot(y) + this.b[i]
-            y = this.act_func[i](z[i])
+        z[0] = this.W[0].dot(x.T) + this.b[0]
+        
+        for l in range(1, L):
+            z[l] = this.W[l].dot(this.act[l-1](z[l-1])) + this.b[l]
 
+        y = this.act[L - 1](z[L - 1])
+        
         return z, y
     
     
     # The much talked about backpropagation
     # 
-    # @x: predictor/input
+    # @x: predictor/input (as ROW VECTOR!)
     # @z: list of the activations for each layer in the network given x as
     #     input
     # @t: targets
     #
     def back_propagate(this, x, z, t):
-        print("This function is not fully implemented yet.")
-        
-        if !this.compiled:
+        if not this.compiled:
             print("Network is not compiled. Use compile().")
             return
         
+        """
+        print("x.shape:")
+        print(x.shape)
+        print("t.shape:")
+        print(t.shape)
+        """
+        
         # setting up neede variables, lists and so on
         L = len(this.W)
-        delta = [None]*len(L)
+        delta = [None]*L
         
         # set up list of gradients with respect to the weights and biases
         gradient_W = []
         gradient_b = []
-        for i in range(L):
-            gradient_W.append(zeros((this.W[i].shape))]
-            gradient_b.append(zeros((this.b[i].shape))]
-        
-
+        for l in range(L):
+            """
+            print("this.W[l].shape:")
+            print(this.W[l].shape)
+            print("this.b[l].shape:")
+            print(this.b[l].shape)
+            """
+            gradient_W.append(zeros((this.W[l].shape)))
+            gradient_b.append(zeros((this.b[l].shape)))
+         
         # Compute delta_L
-        delta[L] += this.cost_func_diff(this.act_func[L](z[L]), t)*this.act_func_diff[L](z[L])
-
-        # set up "iterator"
-        range_L = reversed(range(L))
-        range_L.pop()
-
+        
+        delta[L-1] = this.cost_func_diff(this.act[L-1](z[L-1]), t)*this.act_diff[L-1](z[L-1])
+        
         # Compute delta_l for l=L-1, L-2, ..., 1
-        for l in range_L:
-            delta[l - 1] += sum(delta[l]*this.W[:,l:l+1]
-                    *this.act_func_diff[l-1](z[l-1]), axis=1)
+        for l in range(L-2, -1, -1):
+            da_dz = this.act_diff[l](z[l])
+            delta[l] = this.W[l+1].T.dot(delta[l+1])*this.act_diff[l](z[l])
+        """
+        # special treatment of last iteration
+        print("shape: " )
+        print(delta[1].shape)
+        print(this.W[0].shape)
+        print(x.shape)
+        print((delta[1]*this.W[0].dot(x)).shape)
+        #delta[0] = sum(delta[1]*this.W[0].dot(x), axis=1)
+        """
+
+        # compute the gradient w.r.t. the weights
+        for l in range(L-2, 0, -1):
+            gradient_W[l] = delta[l]*this.act[l-1](z[l-1], t).T # CHECK
+            #gradient_b[l] = delta[l]
         
         # special treatment of last iteration
-        delta[0] += sum(delta[1]*this.W[:,1:2]
-                *x, axis=1)
-
-        # compute the gradients
-        for l in range_L:
-            gradient_W[:,l] = delta[l]*this.act[l-1](z[l-1])).T # CHECK
-            gradient_b[l] = delta[l]
-        
-        # special treatment of last iteration
-        gradient_W[:,0] = delta[0]*x.T # CHECK
-        gradient_b[0] = delta[0]
+        gradient_W[0] = delta[0]*x # CHECK
+        #gradient_b[0] = delta[0]
 
 
-        return gradient_W, gradient_b
+        return gradient_W, delta
 
 
     # computes the gradient with respect to the weights and biases for a 
     # given sample x 
     def compute_gradient(this, x, t):
-
-        z, y = forward(x)
-        gradient_W, gradient_b = back_propagate(this, x, z, t)
+        z, y = this.forward(x)
+        gradient_W, gradient_b = this.back_propagate( x, z, t)
 
         return gradient_W, gradient_b
 
 
     # Fit yor network using stochastic gradient method
     #
-    # @X: trainingdatamatrix, columns are datapoints
-    # @T: targets for training data X, columns are datapoints
+    # @X: trainingdatamatrix, rows are datapoints
+    # @T: targets for training data X, rows are datapoints
     #
     def fit(this, X, T, iters=5, batch_size=10):
-        print("This function is not fully implemented yet.")
-        
-        if !this.compiled:
+        if not this.compiled:
             print("Network is not compiled. Use compile().")
             return
 
-        L = len(this.w)
+        L = len(this.W)
         
         # FIX: send the gradient list as argument to compute_gradient
 
@@ -234,26 +264,26 @@ class NeuralNetwork:
         b_next = []
 
         for l in range(L):
-            gradient_W.append(zeros((this.W[l].shape))]
-            gradient_b.append(zeros((this.b[l].shape))]
-            W_next.append(zeros((this.W[l].shape))]
-            b_next.append(zeros((this.b[l].shape))]
+            gradient_W.append(zeros((this.W[l].shape)))
+            gradient_b.append(zeros((this.b[l].shape)))
+            W_next.append(zeros((this.W[l].shape)))
+            b_next.append(zeros((this.b[l].shape)))
         
         # learn network using stochastic gradient method 
         for i in range(iters):
             # pick random numbers from 0 to x.shape 
-            batch = choice(X.shape[1], batch_size, replace=False)
+            batch = choice(X.shape[0], batch_size, replace=False)
             total_grad_W = 0
             total_grad_b = 0
 
             for j in batch:
                 # compute the gradient of the costfunction for sample j 
-                smpl_grad_W, smpl_grad_b = compute_gradient(X[:,j], T[:,j])
+                smpl_gradient_W, smpl_gradient_b = this.compute_gradient(X[j:j+1,:], T[j:j+1,:])
 
                 # add this gradient/N to the total gradient
                 for l in range(L):
-                    gradient_W[l] += smpl_gradient_W/batch_size
-                    gradient_b[l] += smpl_gradient_b/batch_size
+                    gradient_W[l] += smpl_gradient_W[l]/batch_size
+                    gradient_b[l] += smpl_gradient_b[l]/batch_size
 
             # update weights and biases
             for l in range(L):
@@ -264,5 +294,29 @@ class NeuralNetwork:
 
 
 
+# test program
 
+from numpy import random
+from functions import *
+
+N = 100
+p = 3
+iterations = 50
+
+model = NeuralNetwork()
+
+# set up sample matrix
+X = random.rand(N,p)
+
+# set up targets/responses
+y = random.choice([0,1], N)
+y = y.reshape((N,1))
+
+model.add_layer(5, sigmoid, sigmoid_diff)
+model.add_layer(1, sigmoid, sigmoid_diff)
+model.set_cost_function(cost, cost_diff)
+model.set_inputnodes(p)
+model.compile()
+
+model.fit(X, y, iters=5, batch_size=10)
 
